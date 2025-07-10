5b031e6fd7548b1ffa154bb64e4e1a0f9acb66ed
Parmaryash-7 <parmars4679@gmail.com>
2025-07-10 11:19:41 +0530

    ...Panth

--- next.config.js
+++ next.config.js
@@ -1,18 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   productionBrowserSourceMaps: true,
-  reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone', 
-
   async rewrites() {
     return [
       {
         source: '/',
-        destination: '/home',
-      },
-    ];
-  },
-};
+        destination: '/home'
+      }
+    ]
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
