92eaf4b2cab63756cb70b620e9024d4c9c331ae5
GadaniPanth <gadanipanth@gmail.com>
2025-07-14 10:11:33 +0530

    Start Each Page From top

--- components/AOSInitializer.jsx
+++ components/AOSInitializer.jsx
@@ -1,27 +1,36 @@
-"use client"
+'use client'
 
-import { useEffect } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { useDispatch } from 'react-redux';
+import { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useDispatch } from 'react-redux'
+import { usePathname } from 'next/navigation'
 
 const AOSInitializer = () => {
-//   const country = await getCountryList();
-//   const dispatch = useDispatch()
-    useEffect(() => {
-        AOS.init({
-            duration: 1000,
-            once: true,
-        });
-        
-        return () => {
-            AOS.refresh();
-        };
+  //   const country = await getCountryList();
+  //   const dispatch = useDispatch()
+  const pathname = usePathname()
 
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true
+    })
 
-    }, []);
+    return () => {
+      AOS.refresh()
+    }
+  }, [])
 
-    return null;
-};
+  useEffect(() => {
+    // Scroll to the very top (feel free to set behavior:"smooth")
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' })
 
-export default AOSInitializer;
+    // Tell AOS to recalc element positions
+    // AOS.refreshHard();      // `refreshHard` is safest after layout changes
+  }, [pathname])
+
+  return null
+}
+
+export default AOSInitializer
