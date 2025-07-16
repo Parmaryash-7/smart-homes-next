a56f2e481b0dc158385fef7c9e599dcc754602a9
Parmaryash-7 <parmars4679@gmail.com>
2025-07-16 17:58:46 +0530

    smart homes may be done

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,13 +2,11 @@ import '../public/css/globals.css'
 import Header from 'components/Header'
 import Footer from 'components/Footer'
 import api from 'lib/api.interceptor'
-// import getSocialMediaList from 'lib/SocialMediaList'
-// import getHomeDetail from 'lib/HomeDetail'
 import { Providers } from '../store/Providers'
 import AOSInitializer from '../components/AOSInitializer'
 import InquiryPopup from '../components/InquiryPopup'
 import { ToastContainer } from 'react-toastify'
-
+import Script from 'next/script'
 
 export default async function RootLayout({ children }) {
   const propertylist = await api.Propertylist()
@@ -23,14 +21,43 @@ export default async function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,500,0,0"
         />
         <meta name="robots" content="noindex, nofollow" />
-
-        <script
-          src="https://www.google.com/recaptcha/api.js"
-          async
-          defer
-        ></script>
       </head>
       <body>
+        {/* Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-R31YX2E1Y8"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-R31YX2E1Y8');
+          `}
+        </Script>
+
+        {/* Google Tag Manager Script */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K7HHNXQQ');
+          `}
+        </Script>
+
+        {/* Google reCAPTCHA */}
+        <Script
+          src="https://www.google.com/recaptcha/api.js"
+          strategy="afterInteractive"
+        />
+
         <AOSInitializer />
         <Providers>
           <main>
