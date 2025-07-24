525bf4659f3935fa8f8adf8c2671033715dd0322
Parmaryash-7 <parmars4679@gmail.com>
2025-07-24 18:53:39 +0530

    redirect to sitemap

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   productionBrowserSourceMaps: true,
+
   async rewrites() {
     return [
       {
         source: '/',
         destination: '/home'
+      }
+    ]
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/sitemap.php',
+        destination: '/sitemap',
+        permanent: true, 
       }
     ]
   }
