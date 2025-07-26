7e368b52c1f45cd693c51675170da7a8b9fb5309
jiten1990 <jiten@coronation.in>
2025-07-26 11:53:36 +0530

    Plot booking redirections and robots updated

--- next.config.js
+++ next.config.js
@@ -17,6 +17,51 @@ const nextConfig = {
         source: '/sitemap.php',
         destination: '/sitemap',
         permanent: true, 
+      },
+      {
+        source: '/book-plot-now/dholera-expressway-city-township/',
+        destination: '/dholera-expressway-city-township',
+        permanent: true,
+      },
+      {
+        source: '/book-plot-now/dholera-international-airport-city-1/',
+        destination: '/dholera-international-airport-city-1',
+        permanent: true,
+      },
+      {
+        source: '/book-plot-now/dholera-international-airport-city-2-commercial-plots/',
+        destination: '/dholera-international-airport-city-2-commercial-plots',
+        permanent: true,
+      },
+      {
+        source: '/book-plot-now/dholera-forest-estate/',
+        destination: '/dholera-forest-estate',
+        permanent: true,
+      },
+      {
+        source: '/book-plot-now/dholera-expressway-city-I/',
+        destination: '/dholera-expressway-city-I',
+        permanent: true,
+      },
+      {
+        source: '/book-plot-now/dholera-expressway-avenue-1-commercial-plots/',
+        destination: '/dholera-expressway-avenue-1-commercial-plots',
+        permanent: true,
+      },
+      {
+        source: '/book-plot-now/dholera-forest-estate-commercial-plots/',
+        destination: '/dholera-forest-estate-commercial-plots',
+        permanent: true,
+      },
+      {
+        source: '/book-plot-now/dholera-international-airport-city-2/',
+        destination: '/dholera-international-airport-city-2',
+        permanent: true,
+      },
+      {
+        source: '/book-plot-now/dholera-expressway-avenue-1/',
+        destination: '/dholera-expressway-avenue-1',
+        permanent: true,
       }
     ]
   }
