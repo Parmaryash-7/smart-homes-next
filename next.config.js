aa9bc7663d3dbe30c25bd219554c8e8ced43ed0b
Parmaryash-7 <parmars4679@gmail.com>
2025-07-29 20:00:41 +0530

    redirection done

--- next.config.js
+++ next.config.js
@@ -23,6 +23,101 @@ const nextConfig = {
         destination: '/dholera-forest-estate',
         permanent: true, 
       },
+       {
+        source: '/article/dholera-smart-city-news-by-smart-homes-vol-06',
+        destination: '/images/pdf/DHOLERA-SMART-CITY-NEWS-BY-SMART-HOMES-VOL-05.pdf',
+        permanent: true, 
+      },
+       {
+        source: '/project/mulberry-park',
+        destination: '/mulberry-park',
+        permanent: true, 
+      },
+       {
+        source: '/project/abcd-enclave-4',
+        destination: '/abcd-enclave-4',
+        permanent: true, 
+      },
+       {
+        source: '/project/maple-garden',
+        destination: '/maple-garden',
+        permanent: true, 
+      },
+       {
+        source: '/article/empowering-tomorrow-pm-modis-semiconductor-vision-ignites-dholera-smart-citys-growth',
+        destination: '/blogs/empowering-tomorrow-pm-modis-semiconductor-vision-ignites-dholera-smart-citys-growth',
+        permanent: true, 
+      },
+       {
+        source: '/project/mulberry-park/type=construction',
+        destination: '/mulberry-park',
+        permanent: true, 
+      },
+       {
+        source: '/project/abcd-enclave-3/type=gallery',
+        destination: '/abcd-enclave-3',
+        permanent: true, 
+      },
+       {
+        source: '/project/piccadilly-square-2',
+        destination: '/piccadilly-square-2',
+        permanent: true, 
+      },
+       {
+        source: '/project/olive-garden/type=construction',
+        destination: '/olive-garden',
+        permanent: true, 
+      },
+       {
+        source: '/nri-section.html',
+        destination: '/nri-corner',
+        permanent: true, 
+      },
+       {
+        source: '/project/abcd-greens-1',
+        destination: '/abcd-greens-1',
+        permanent: true, 
+      },
+       {
+        source: '/project/orange-county/type=gallery',
+        destination: '/orange-county',
+        permanent: true, 
+      },
+       {
+        source: '/project/abcd-enclave-I/type=gallery',
+        destination: '/abcd-enclave-I',
+        permanent: true, 
+      },
+       {
+        source: '/project/abcd-greens-1/type=gallery',
+        destination: '/abcd-greens-1',
+        permanent: true, 
+      },
+       {
+        source: '/project/abcd-enclave-I',
+        destination: '/abcd-enclave-I',
+        permanent: true, 
+      },
+       {
+        source: '/file/Draft-DPR-for-development-of-Dholera-Solar-Park.pdf',
+        destination: '/images/document-pdf/Draft-DPR-for-development-of-Dholera-Solar-Park.pdf',
+        permanent: true, 
+      },
+       {
+        source: '/file/prelim-dholerasir-town-planning-4A-OPFP.pdf',
+        destination: '/images/document-pdf/prelim-dholerasir-town-planning-4A-OPFP.pdf',
+        permanent: true, 
+      },
+       {
+        source: '/file/ABCD-ENCLAVE-3_Bhadiyad-286-Gam-no-6-hak-patrak.pdf',
+        destination: '/abcd-enclave-3',
+        permanent: true, 
+      },
+       {
+        source: '/file/Ambli-665-Brochure-Website.pdf',
+        destination: '/important-documents',
+        permanent: true, 
+      },
       // {
       //   source: '/book_plot/dholera-expressway-city-township/',
       //   destination: '/dholera-expressway-city-township',
